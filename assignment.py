70ce07e8afedcca270df75e08f9e8447ae8e13d4
Nicole Hellen <260287479+hellennicole@users.noreply.github.com>
2026-03-13 13:26:15 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,5 +1,24 @@
 #Section B 
-##Question 5
+#Question 4
 print("Hello AI Students")
 
+#Question 5
+name = "Nicole"
+age = 21
+city = "Nairobi"
+print("Name:",name)
+print("Age:",age)
+print("City:",city)
 
+#Question 6
+programming_languages = ["Python", "Java", "C++", "JavaScript", "Go"]
+  print(programming_languages[0])
+
+#Question 7
+student = {
+    "name": "Nicole",
+    "age": 21,
+    "course": "AI"
+}
+ 
+print(student["name"])
